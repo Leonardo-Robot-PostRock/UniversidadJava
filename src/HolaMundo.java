bf3aff25419968de35803e465924efd537494555
Leonardo <leonardo38361@gmail.com>
2023-10-18 03:43:07 -0300

    Variables de tipo string

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,5 +4,16 @@ public class HolaMundo {
     public static void main(String[] args) {
         int miVariableEntera = 10;
         System.out.println(miVariableEntera);
+
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+
+        String miVariableCadena = "Argentina";
+
+        System.out.println(miVariableCadena);
+
+        miVariableCadena = "Mendoza";
+
+        System.out.println(miVariableCadena);
     }
 }
