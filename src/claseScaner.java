ef56572a06c4213c9ade15ad31f82713eac2efda
Leonardo <leonardo38361@gmail.com>
2023-11-16 13:14:04 -0300

    Uso de la clase scanner

---
+++ src/claseScaner.java
@@ -0,0 +1,18 @@
+package src;
+//Dentro del paquete útil va importar la clase Scanner para poder utilizarla
+import java.util.Scanner;
+
+public class claseScaner {
+    public static void main(String[] args) {
+        System.out.println("Escribe tu nombre:");
+        //Leer información de la consola
+        //System clase de Java
+        //In de input
+        Scanner consola = new Scanner(System.in);
+        var usuario = consola.nextLine();
+        System.out.println("Usuario = " + usuario);
+        System.out.println("Escribe el título: ");
+        var titulo = consola.nextLine();
+        System.out.println("Resultado: " + titulo + " " + usuario);
+    }
+}
