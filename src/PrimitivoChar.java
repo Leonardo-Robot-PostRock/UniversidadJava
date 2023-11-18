0185fab60b67e78aef0c8db351f2b1b5f25e1322
Leonardo <leonardo38361@gmail.com>
2023-11-17 23:17:44 -0300

    tipo primitivo char

---
+++ src/PrimitivoChar.java
@@ -0,0 +1,32 @@
+package src;
+
+public class PrimitivoChar {
+    public static void main(String[] args) {
+        char miCaracter = 'a';
+        System.out.println("miCaracter = " + miCaracter);
+
+        char varChar = '\u0021';
+        System.out.println("varChar = " + varChar);
+
+        char varCharDecimal = 33;
+        System.out.println("varCharDecimal = " + varCharDecimal);
+
+        char varCharSimbolo = '!';
+        System.out.println("varCharSimbolo = " + varCharSimbolo);
+
+        var varChar1 = '\u0021';
+        System.out.println("varChar = " + varChar1);
+
+        var varChar2Decimal = 33;
+        System.out.println("varCharDecimal = " + varChar2Decimal);
+
+        var varChar3Simbolo = '!';
+        System.out.println("varCharSimbolo = " + varChar3Simbolo);
+
+        int variableEnteraSimbolo = '!';
+        System.out.println("variableEnteraSimbolo = " + variableEnteraSimbolo);
+        
+        int letra = 'A';
+        System.out.println("letra = " + letra);
+    }
+}
