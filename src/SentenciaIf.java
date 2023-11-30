451bcf6b808863dbdb643ca42bd91400e335be93
Leonardo <leonardo38361@gmail.com>
2023-11-30 20:52:10 -0300

    Sentencia if

---
+++ src/SentenciaIf.java
@@ -0,0 +1,15 @@
+package src;
+
+//Mi clase en Java
+public class SentenciaIf {
+
+    public static void main(String args[]) {
+        var condicion = true;
+
+        if (condicion) {
+            System.out.println("Condicion verdadera");
+        } else {
+            System.out.println("Condicion falsa");
+        }
+    }
+}
