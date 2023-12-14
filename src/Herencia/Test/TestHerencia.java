e376b9884e0cbe7a57b37d464382c3acc03c9cdb
Leonardo <leonardo38361@gmail.com>
2023-12-14 03:33:52 -0300

    testeando clase Cliente

--- src/Herencia/Test/TestHerencia.java
+++ src/Herencia/Test/TestHerencia.java
@@ -1,10 +1,18 @@
 package src.Herencia.Test;
 
+import src.Herencia.domain.Cliente;
 import src.Herencia.domain.Empleado;
+
+import java.util.Date;
 
 public class TestHerencia {
     public static void main(String[] args) {
         Empleado empleado1 = new Empleado("Leonardo", 3000);
         System.out.println("empleado1 = " + empleado1);
+
+        Date currentDate = new Date();
+
+        Cliente cliente = new Cliente("Yamila", currentDate, true);
+        System.out.println("cliente = " + cliente);
     }
 }
