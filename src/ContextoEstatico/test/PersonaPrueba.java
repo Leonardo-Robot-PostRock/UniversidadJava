3cc29e4651be431ce8ad661041afd13bd08c631f
Leonardo <leonardo38361@gmail.com>
2023-12-13 23:24:29 -0300

    se agrega comentario

--- src/ContextoEstatico/test/PersonaPrueba.java
+++ src/ContextoEstatico/test/PersonaPrueba.java
@@ -4,6 +4,9 @@ import src.ContextoEstatico.domain.Persona;
 
 public class PersonaPrueba {
     public static void main(String[] args) {
+        //El contexto estático se crea primero que el contexto dinámico y por ello no se puede acceder a this.
+        //Por otro lado no se puede acceder a ningun método que no sea estático.
+        //Por otro lado un método dinámico si puede acceder a un método estático ya que este ha sido creado.
         Persona persona1 = new Persona("Leonardo");
         System.out.println("persona1 = " + persona1);
         
