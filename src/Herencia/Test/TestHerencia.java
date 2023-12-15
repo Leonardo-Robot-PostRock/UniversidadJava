33afee56abd693372af76738a009448f70644416
Leonardo <leonardo38361@gmail.com>
2023-12-15 02:36:15 -0300

    probando sobrecarga de constructores

--- src/Herencia/Test/TestHerencia.java
+++ src/Herencia/Test/TestHerencia.java
@@ -1,18 +1,15 @@
 package src.Herencia.Test;
 
-import src.Herencia.domain.Cliente;
 import src.Herencia.domain.Empleado;
-
-import java.util.Date;
 
 public class TestHerencia {
     public static void main(String[] args) {
         Empleado empleado1 = new Empleado("Leonardo", 3000);
         System.out.println("empleado1 = " + empleado1);
 
-        var fecha = new Date();
-
-        Cliente cliente = new Cliente("Yamila", 'f', 27, "Guaymallén", fecha, true);
-        System.out.println("cliente = " + cliente);
+//        var fecha = new Date();
+//
+//        Cliente cliente = new Cliente("Yamila", 'f', 27, "Guaymallén", fecha, true);
+//        System.out.println("cliente = " + cliente);
     }
 }
