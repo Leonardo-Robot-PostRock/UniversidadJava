b0c3b80570ac60e15c07ebf6982361d74f4baefa
Leonardo <leonardo38361@gmail.com>
2023-12-15 03:13:53 -0300

    Ejercicio de paquetes

---
+++ src/Paquetes/mx/com/globalmentoring/Utileria.java
@@ -0,0 +1,7 @@
+package src.Paquetes.mx.com.globalmentoring;
+
+public class Utileria {
+    public static void imprimir(String s){
+        System.out.println("s = " + s);
+    }
+}
---
+++ src/Paquetes/test/TestUtileria.java
@@ -0,0 +1,12 @@
+package src.Paquetes.test;
+
+//import src.Paquetes.mx.com.globalmentoring.Utileria;
+//import src.Paquetes.mx.com.globalmentoring.*;
+//import static src.Paquetes.mx.com.globalmentoring.Utileria.imprimir;
+public class TestUtileria {
+    public static void main(String[] args) {
+        //Utileria.imprimir("Saludos");
+        //imprimir("Adiós");
+        src.Paquetes.mx.com.globalmentoring.Utileria.imprimir("adiós");
+    }
+}
