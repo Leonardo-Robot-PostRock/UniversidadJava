6c2b19052074d34e190921c21465266dac28602c
Leonardo <leonardo38361@gmail.com>
2023-12-15 02:55:51 -0300

    prueba de sobrecarga de metodos

--- src/SobrecargaMetodos/test/TestOperaciones.java
+++ src/SobrecargaMetodos/test/TestOperaciones.java
@@ -1,2 +1,16 @@
-package src.SobrecargaMetodos.test;public class TestOperaciones {
+package src.SobrecargaMetodos.test;
+
+import src.SobrecargaMetodos.operaciones.Operaciones;
+
+public class TestOperaciones {
+    public static void main(String[] args) {
+        var resultado = Operaciones.sumar(5, 4);
+        System.out.println("resultado = " + resultado);
+
+        var resultado2 = Operaciones.sumar(2.0, 3);
+        System.out.println("resultado = " + resultado2);
+        
+        var resultado3 = Operaciones.sumar(3, 5L);
+        System.out.println("resultado3 = " + resultado3);
+    }
 }
