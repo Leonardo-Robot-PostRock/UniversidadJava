448e5e10141b488cf996439df04a051bc3786d67
Leonardo <leonardo38361@gmail.com>
2024-05-20 17:13:40 -0300

    acceso public

--- src/ModificadoresDeAcceso/paquete1/Clase1.java
+++ src/ModificadoresDeAcceso/paquete1/Clase1.java
@@ -1,2 +1,13 @@
-package src.ModificadoresDeAcceso.paquete1;public class Clase1 {
+package src.ModificadoresDeAcceso.paquete1;
+
+public class Clase1 {
+    public String atributoPublico = "Valor atributo público";
+
+    public Clase1(){
+        System.out.println("Constructor Público");
+    }
+
+    public void metodoPublico(){
+        System.out.println("Método Público");
+    }
 }
--- src/ModificadoresDeAcceso/test/TestModificadoresAcceso.java
+++ src/ModificadoresDeAcceso/test/TestModificadoresAcceso.java
@@ -1,2 +1,14 @@
-package src.ModificadoresDeAcceso.test;public class TestModificadoresAcceso {
+package src.ModificadoresDeAcceso.test;
+
+import src.ModificadoresDeAcceso.paquete1.Clase1;
+
+public class TestModificadoresAcceso {
+    public static void main(String[] args) {
+        Clase1 clase1 = new Clase1();
+
+        System.out.println("clase1: " + clase1.atributoPublico);
+
+        clase1.metodoPublico();
+
+    }
 }
