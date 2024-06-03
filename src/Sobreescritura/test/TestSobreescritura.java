3f5992762cbd3292610f20ca6e66b053624ee84c
Leonardo <leonardo38361@gmail.com>
2024-06-03 00:41:09 -0300

    cambio de ruta de archivos

--- src/Sobreescritura/test/TestSobreescritura.java
+++ src/Sobreescritura/test/TestSobreescritura.java
@@ -1,10 +1,10 @@
-package src.Sobreescritura.domain;
+package src.Sobreescritura.test;
+
+import src.Sobreescritura.domain.Gerente;
 
 public class TestSobreescritura {
     public static void main(String[] args) {
         Gerente gerente1 = new Gerente("Juan", 5000, "Sistemas");
         System.out.println("gerente 1:\n" + gerente1.obtenerDetalles());
-
-
     }
 }
