c0a19e3597634a4226b9a8d5ad444462f12742be
Leonardo <leonardo38361@gmail.com>
2024-06-05 01:24:06 -0300

    Api collections

---
+++ src/ApiCollecciones/test/TestCollecciones.java
@@ -0,0 +1,50 @@
+package src.ApiCollecciones.test;
+
+import java.util.*;
+
+public class TestCollecciones {
+    public static void main(String[] args) {
+        //El método add() esta definido en la interface padre, es decir en Collection.
+
+
+        List miLista = new ArrayList<>();
+        miLista.add("Lunes");
+        miLista.add("Martes");
+        miLista.add("Miércoles");
+        miLista.add("Jueves");
+        miLista.add("Viernes");
+        miLista.add("Viernes");
+
+        imprimir(miLista);
+        System.out.println("\n");
+//        for (Object list : miLista) {
+//            System.out.println("elemento = " + list);
+//        }
+
+        //funciones lambda o funciones flecha
+//        miLista.forEach(elemento -> {
+//            System.out.println("elemento = " + elemento);
+//        });
+
+        //No se respeta el orden para agregar objetos. Aunque es más rápido para agregarlos.
+        //No se puede duplicar elementos.
+        Set miSet = new HashSet();
+        miSet.add("Lunes");
+        miSet.add("Martes");
+        miSet.add("Miércoles");
+        miSet.add("Jueves");
+        miSet.add("Viernes");
+        miSet.add("Viernes");
+
+        imprimir(miSet);
+    }
+
+    public static void imprimir(Collection collection) {
+//        for (Object elemento : collection) {
+//            System.out.println("elemento = " + elemento);
+//        }
+        collection.forEach(elemento -> {
+            System.out.println("elemento = " + elemento);
+        });
+    }
+}
